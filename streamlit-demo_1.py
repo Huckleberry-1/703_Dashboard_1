761023d2ac8a97f8c208720e05c8faa3207ec8ea
Huckleberry-1 <130390629+Huckleberry-1@users.noreply.github.com>
2023-04-10 19:59:36 -0400

    Indent_Plotly

--- streamlit-demo_1.py
+++ streamlit-demo_1.py
@@ -105,18 +105,18 @@ with st.container():
 
 with st.container()
 # Add histogram data
-x1 = np.random.randn(200) - 2
-x2 = np.random.randn(200)
-x3 = np.random.randn(200) + 2
+    x1 = np.random.randn(200) - 2
+    x2 = np.random.randn(200)
+    x3 = np.random.randn(200) + 2
 
 # Group data together
-hist_data = [x1, x2, x3]
+    hist_data = [x1, x2, x3]
 
-group_labels = ['Group 1', 'Group 2', 'Group 3']
+    group_labels = ['Group 1', 'Group 2', 'Group 3']
 
 # Create distplot with custom bin_size
-fig = ff.create_distplot(hist_data, group_labels, bin_size=[.1, .25, .5])
+    fig = ff.create_distplot(hist_data, group_labels, bin_size=[.1, .25, .5])
 
 # Plot!
-st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig, use_container_width=True)
 

cdf4014552fa45fa555de8975b870266f9d5b0ff
Huckleberry-1 <130390629+Huckleberry-1@users.noreply.github.com>
2023-04-10 19:58:10 -0400

    Added_Plotly

--- streamlit-demo_1.py
+++ streamlit-demo_1.py
@@ -9,6 +9,7 @@ import numpy as np
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import plotly.express as px
+import plotly.figure_factory as ff
 
 
 
@@ -101,3 +102,21 @@ with col2:
 with st.container():    
     plt.title(title)
     st.pyplot(fig)
+
+with st.container()
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
+
